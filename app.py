f98873cc49ebcc2611f8894335f5be7b16c0d919
santimontironi <santiimontironi@gmail.com>
2025-02-22 23:31:11 -0300

    blueprints registrados en app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, session, redirect, url_for
 from models.bd import inicializar_bd
 from models.mail import configurar_mail
+from routes.login import loginEmpleado_bp
+from routes.empleados import empleados_bp
+from routes.clientes import clientes_bp
+from routes.agregarClientes import agregarClientes_bp
+from routes.editarClientes import editarCliente_bp
+from routes.eliminarClientes import eliminarCliente_bp
+from routes.noticiasRecientes import noticiasRecientes_bp
+from routes.sugerencias import sugerencias_bp
+from routes.misSugerencias import misSugerencias_bp
 
 #SE GENERA UNA CLAVE DE FLASK Y EL ENTORNO DE TRABAJO DE LA APP.
 app = Flask(__name__)
@@ -11,6 +20,17 @@ mysql = inicializar_bd(app)
 
 #INICIALIZACION DEL SERVIDOR DE MAILS
 Mail = configurar_mail(app)
+
+#SE REGISTRAN LOS BLUEPRINTS
+app.register_blueprint(loginEmpleado_bp)
+app.register_blueprint(empleados_bp)
+app.register_blueprint(clientes_bp)
+app.register_blueprint(agregarClientes_bp)
+app.register_blueprint(editarCliente_bp)
+app.register_blueprint(eliminarCliente_bp)
+app.register_blueprint(noticiasRecientes_bp)
+app.register_blueprint(sugerencias_bp)
+app.register_blueprint(misSugerencias_bp)
 
 #RUTA Y FUNCION DEL INICIO
 @app.route('/')
